4265ef0e1ae20d54de8dc3aea02eadf3b7cd0551
Nikolozi11 <nikolkzgazviani2011@gmail.com>
2025-06-15 14:11:32 +0400

    second commit

--- function.js
+++ function.js
@@ -1,6 +1,16 @@
+//nav
 const navToggle = document.getElementById('navToggle');
-  const navLinks = document.getElementById('navLinks');
+const navLinks = document.getElementById('navLinks');
 
-  navToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-  });
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+//gazeti
+    const btn = document.getElementById('toggleBtn');
+    const desc = document.getElementById('desc');
+
+    btn.addEventListener('click', () => {
+      const expanded = desc.classList.toggle('expanded');
+      btn.textContent = expanded ? 'მოკლედ' : 'დაწვრილებით';
+    });
